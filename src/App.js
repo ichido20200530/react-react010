75c87dac52b2d701710e8629053b64913700220f
ichido <ichido@live.jp>
2020-06-11 07:21:42 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  input = '';
+
+  msgStyle = {
+    fontSize: "20pt",
+    color: "#900",
+    margin: "20px 0px",
+    padding: "5px",
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: 'type your name:'
+    };
+    this.doCheck = this.doCheck.bind(this);
+  }
+
+  doCheck(event) {
+    alert(event.target.value + "は長すぎます。(最大10文字)");
+  }
+
+  render() {
+    return <div>
+      <h1>React</h1>
+      <h2>{this.state.message}</h2>
+      <Message maxlength="10" onCheck={this.doCheck} />
+    </div>;
+  }
+
 }
 
-export default App;
+class Message extends Component {
+  inputStyle = {
+    fontSize: "12pt",
+    padding: "5px",
+  }
+
+  constructor(props) {
+    super(props);
+    this.doChange = this.doChange.bind(this);
+  }
+
+  doChange(e) {
+    if (e.target.value.length > this.props.maxlength) {
+      this.props.onCheck(e);
+      e.target.value = e.target.value.substr(0,this.props.maxlength);
+    }
+  }
+
+  render() {
+    return <input type="text" style={this.inputStyle}
+      onChange={this.doChange} />
+  }
+}
